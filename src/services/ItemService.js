d1c1b7e9f4ace8102ed935f09f26e89e33ec0d6f
Saif  Billah <saif.billah@gmail.com>
2019-12-07 11:47:18 -0500

    Created item service.

---
+++ src/services/ItemService.js
@@ -0,0 +1,78 @@
+export default class ItemService {
+	
+	static myInstance = null;
+
+    static getInstance() {
+    if (ItemService.myInstance == null) {
+        ItemService.myInstance = new ItemService() 
+    }
+        return this.myInstance
+    }
+
+    EbayURL = 'https://peaceful-caverns-80012.herokuapp.com/api/'
+
+    // Return array of item objects
+	getAllItems(callback) {
+        fetch(this.EbayURL + 'items')
+        .then(response => response.json())
+        .then(allItems => callback(allItems))
+    }
+    
+    // Return one item object
+    findItemById(itemId, callback) {
+        fetch(this.EbayURL + 'items/' + itemId)
+        .then(response => response.json())
+        .then(item => callback(item))
+    }
+
+    // Return an Item object if successful
+    // NOTE: Not sure if the .then() clauses are set up correctly
+    createItemForUser(userId, item, callback) {
+        fetch(this.EbayURL + 'users/' + userId + '/items', {
+            method: 'post', 
+            body: JSON.stringify(item),
+            headers: {
+                'content-type': 'application/json'   }
+            })
+            .then(response => response.json())
+            .then(item => callback(item))
+    }
+
+    // Return a list of item objects by keyword
+    findItemsByKeyword(keyword, callback) {
+        fetch(this.EbayURL + 'searchitems/' + keyword)
+        .then(response => response.json())
+        .then(itemList => callback(itemList))
+    }
+
+    // Delete Item by Id
+    // Does not return anything.
+    deleteItemById(itemId) {
+        fetch(this.EbayURL + 'items/' + itemId, {
+            method: 'delete', 
+            headers: {
+                'content-type': 'application/json'   }
+            })
+    }
+
+    // Update an item via itemid
+    // Return newly updated item object
+    updateItem(item, itemId, callback) {
+        fetch(this.EbayURL + 'items/' + itemId, {
+            method: 'put', 
+            body: JSON.stringify(item),
+            headers: {
+                'content-type': 'application/json'   }
+            })
+            .then(response => response.json())
+            .then(item => callback(item))
+    }
+
+    // Find number of bookmarks for a given item
+    // Return integer number of bookmarks
+    findNumberOfBookmarksForItem(itemId, callback) {
+        fetch(this.EbayURL + 'items/' + itemId + '/bookmarks')
+        .then(response => console.log("Number of Bookmarks for Item: " + response))
+        .then(numberOfBookmarks => callback(numberOfBookmarks))
+    }
+}
